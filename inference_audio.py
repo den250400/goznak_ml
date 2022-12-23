d5b01ce55d5db417edff7c418c02281c01a567bf
den250400 <den250400@gmail.com>
2022-12-23 17:27:18 +0700

    Refactored inference scripts

--- inference_audio.py
+++ inference_audio.py
@@ -1,32 +1,25 @@
 import torch
+import argparse
 import os
 
 from modules.models import LSTMDenoiser
-from modules.dataloader import load_denoising_dataloader
-import config
 from modules.audioutils import mel2audio, audio2mel
+import config
 
-PATH = "./data/noisy.wav"
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--input_path", type=str, help="Path to input audio file", default='./data/noisy.wav')
+parser.add_argument("--model_filename", type=str, help="Filename of model state dict", default='denoiser.pth')
+parser.add_argument("--output_path", type=str, help="Output audio save path", default='./data/predicted.wav')
+args = parser.parse_args()
+
 
 model = LSTMDenoiser(input_dim=80, hidden_dim=80, spectrogram_resolution=80)
-model.load_state_dict(torch.load(os.path.join(config.MODEL_SAVE_DIR, 'denoiser.pth')))
+model.load_state_dict(torch.load(os.path.join(config.MODEL_SAVE_DIR, args.model_filename)))
 
-
-spectrogram, samplerate = audio2mel(PATH)
+spectrogram, samplerate = audio2mel(args.input_path)
 spectrogram_tensor = torch.tensor(spectrogram, dtype=torch.float).unsqueeze(1)
 
 predicted = model(spectrogram_tensor)
 
-mel2audio(predicted.detach().squeeze().numpy().T, "./data/predicted.wav", samplerate=samplerate)
-
-"""
-dataloader = load_denoising_dataloader("./data/val")
-
-for noisy, clean in dataloader:
-    predicted = model(noisy.permute(1, 0, 2))
-
-    mel2audio(clean.squeeze().numpy().T, "./data/clean.wav")
-    mel2audio(noisy.squeeze().numpy().T, "./data/noisy.wav")
-    mel2audio(predicted.detach().squeeze().numpy().T, "./data/predicted.wav")
-    break
-"""
+mel2audio(predicted.detach().squeeze().numpy().T, args.output_path, samplerate=samplerate)
---
+++ inference_from_dataset.py
@@ -0,0 +1,30 @@
+import torch
+import argparse
+import os
+
+from modules.models import LSTMDenoiser
+from modules.dataloader import load_denoising_dataloader
+from modules.audioutils import mel2audio
+import config
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--data_path", type=str, help="Path to directory with test data", default='./data/val')
+parser.add_argument("--model_filename", type=str, help="Filename of model state dict", default='denoiser.pth')
+parser.add_argument("--clean_path", type=str, help="Clean audio save path", default='./data/clean.wav')
+parser.add_argument("--noisy_path", type=str, help="Noisy audio save path", default='./data/noisy.wav')
+parser.add_argument("--predicted_path", type=str, help="Predicted (denoised) audio save path", default='./data/predicted.wav')
+args = parser.parse_args()
+
+
+model = LSTMDenoiser(input_dim=80, hidden_dim=80, spectrogram_resolution=80)
+model.load_state_dict(torch.load(os.path.join(config.MODEL_SAVE_DIR, args.model_filename)))
+
+dataloader = load_denoising_dataloader(args.data_path)
+
+for noisy, clean in dataloader:
+    predicted = model(noisy.permute(1, 0, 2))
+
+    mel2audio(clean.squeeze().numpy().T, args.clean_path)
+    mel2audio(noisy.squeeze().numpy().T, args.noisy_path)
+    mel2audio(predicted.detach().squeeze().numpy().T, args.predicted_path)
+    break
